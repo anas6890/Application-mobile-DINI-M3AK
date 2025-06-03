18e33d0faea35b0d0e9e4690446a561008eca611
Amine <amine.belh204@gmail.com>
2025-06-03 21:14:17 +0200

    transitions

--- app/src/main/java/com/example/dinim3ak/Page19Activity.java
+++ app/src/main/java/com/example/dinim3ak/Page19Activity.java
@@ -1,6 +1,8 @@
 package com.example.dinim3ak;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +11,22 @@ public class Page19Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.page19);
+    }
+
+    public void goBack(View view){
+        finish();
+    }
+    public void goToPassager(View view){
+
+    }
+    public void goToConducteur(View view){
+        Intent i = new Intent(this, Page14Activity.class);
+        this.startActivity(i);
+    }
+    public void goToCovoiturage(View view){
+
+    }
+    public void goToWallet(View view){
+
     }
 }
--- app/src/main/java/com/example/dinim3ak/Page21Activity.java
+++ app/src/main/java/com/example/dinim3ak/Page21Activity.java
@@ -1,6 +1,8 @@
 package com.example.dinim3ak;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +11,21 @@ public class Page21Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.page21);
+    }
+    public void goBack(View view){
+        finish();
+    }
+    public void goToPassager(View view){
+
+    }
+    public void goToConducteur(View view){
+        Intent i = new Intent(this, Page14Activity.class);
+        this.startActivity(i);
+    }
+    public void goToCovoiturage(View view){
+
+    }
+    public void goToWallet(View view){
+
     }
 }
