205293d0ebd434ed267bab629982b6f1dbe13efb
anas6890 <anas.boucetta@student.emi.ac.ma>
2025-06-04 00:54:37 +0100

    last update

--- app/src/main/java/com/example/dinim3ak/SplashActivity.java
+++ app/src/main/java/com/example/dinim3ak/SplashActivity.java
@@ -11,7 +11,7 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.splash_screen);
+        setContentView(R.layout.page1);
         new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
             @Override
             public void run() {
