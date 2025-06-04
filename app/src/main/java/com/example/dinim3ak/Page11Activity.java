d4a154b08964a5ed42598553916d942c390a2203
Amine <amine.belh204@gmail.com>
2025-06-04 19:36:05 +0200

    added page11

---
+++ app/src/main/java/com/example/dinim3ak/Page11Activity.java
@@ -0,0 +1,18 @@
+package com.example.dinim3ak;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Page11Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.page11);
+    }
+}
